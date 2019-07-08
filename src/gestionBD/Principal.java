76adaa8ae8f69e2a15b7d33ba41440c258949350
svargas-sys <51766047+svargas-sys@users.noreply.github.com>
2019-07-08 14:43:54 -0400

    UPDATE

--- src/gestionBD/Principal.java
+++ src/gestionBD/Principal.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//CodePlay is licensed under the
+//
+//Apache License 2.0
+//A permissive license whose main conditions require preservation of 
+//copyright and license notices. Contributors provide an express grant 
+//of patent rights. Licensed works, modifications, and larger works 
+//may be distributed under different terms and without source code.
+//
+//creadores
+//CARLOS CERDA
+//JOSE LOBOS
+//SERGIO VARGAS
+//JOSE MUÑOZ
 package gestionBD;
 
 import javax.swing.UIManager;
